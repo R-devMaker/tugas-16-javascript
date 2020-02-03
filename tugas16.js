fc62df60701b2bdd9aa5d73361f2047cf0270d76
R-devMaker <rei.forworkonly@gmail.com>
2020-02-03 14:20:23 +0700

    Tugas 16 JavaScript
    Pengumpulan tugas 16 JavaScript Niomic

---
+++ tugas16.js
@@ -0,0 +1,9 @@
+var bag1min = Math.min(45, 100, 99, 3, 2, 4, 1, 2, 3, 19, 22, 14, 10);
+console.log("Nilai Minimal : ",bag1min);
+var bag1max = Math.max(45, 100, 99, 3, 2, 4, 1, 2, 3, 19, 22, 14, 10);
+console.log("Nilai Maximal : ",bag1max);
+console.log();
+var bag2min = Math.min(45, 100, 99, 3, 2, 81, 22, 55, 1, 2, 3, 4, 5);
+console.log("Nilai Minimal : ",bag1min);
+var bag2max = Math.max(45, 100, 99, 3, 2, 81, 22, 55, 1, 2, 3, 4, 5);
+console.log("Nilai Maximal : ",bag2max);
